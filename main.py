82204c6ab4736ab07e48cdbf9047bdee3e6a28e6
yunseobyuk <ys.yuk@gmail.com>
2024-11-02 09:18:29 +0000

    add learing based checkoutpoint

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         # torchvision.transforms.Resize((112, 112)),
         torchvision.transforms.ToTensor(),
         # torchvision.transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)),
-        torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),git 
         torchvision.transforms.RandomHorizontalFlip()
         ])
 
